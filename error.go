20e6595b53f485f129a7f85b7dec63b5b573fb5d
Bale <balemail@163.com>
2024-12-26 16:37:33 +0800

    Feat 增加错误重试

--- error.go
+++ error.go
@@ -1,6 +1,9 @@
 package util
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func RecoverForPanic() {
 	if err := recover(); err != nil {
@@ -11,4 +14,25 @@ func RecoverForPanic() {
 func RunWithRecover(f func()) {
 	defer RecoverForPanic()
 	f()
+}
+
+type RetryFunc = func() error
+
+// ErrorRetry 错误重试
+func ErrorRetry(maxRetry int, rf RetryFunc) (err error) {
+	return ErrorRetryWithInterval(maxRetry, rf, 0)
+}
+
+func ErrorRetryWithInterval(maxRetry int, rf RetryFunc, interval time.Duration) (err error) {
+	if maxRetry <= 0 {
+		return
+	}
+	for range maxRetry {
+		err = rf()
+		if err == nil {
+			return
+		}
+		time.Sleep(interval)
+	}
+	return
 }
